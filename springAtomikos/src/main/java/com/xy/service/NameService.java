e369214200c987f6509c5449185979ee61b65cb7
skybj <13681568406@163.com>
2018-08-03 21:37:18 +0800

    1111

--- springAtomikos/src/main/java/com/xy/service/NameService.java
+++ springAtomikos/src/main/java/com/xy/service/NameService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Created by 
+ * Created by 11
  */
 @Service
 public class NameService {
